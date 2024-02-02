9b899d2c4dd69d49c7dde3957cf73912afe61696
ArvoVesi <arvo.vesi@tktk.ee>
2024-02-02 05:32:39 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See on kommentaar
+
+// Rakendus küsib kasutaja nime
+// Rakendus tervitab kasutajat
+
+Console.WriteLine("Enter your name:");
+// String - sõne
+string userName = Console.ReadLine(); // Input
+
+// String interpolation
+Console.WriteLine($"Hello, {userName} ! "); // Output
